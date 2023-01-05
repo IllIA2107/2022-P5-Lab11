79db8abf6fa7d5335aedec0822c37d4dab7c5594
IllIA2107 <illia.karmazin@vtsd.com>
2023-01-05 13:12:02 -0500

    lab11

--- Lab11avst.java
+++ Lab11avst.java
@@ -3,8 +3,8 @@
 // This is the student, starting version of the Lab11a assignment.
 
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
-
 
 public class Lab11avst
 {
@@ -12,7 +12,8 @@ public class Lab11avst
     {
         // This main method needs additions for the 100 point version.
         Scanner input = new Scanner(System.in);
-        final int MAX = 100;
+        System.out.print("Enter the primes upper bond  ==>>  ");
+        final int MAX = input.nextInt();
         boolean primes[];
         primes = new boolean[MAX];
         computePrimes(primes);
@@ -21,14 +22,47 @@ public class Lab11avst
 
     public static void computePrimes(boolean primes[])
     {
+        for(int k = 0; k < primes.length; k++) {
+            primes[k] = true;
+        }
+        outer:
+            for(int k = 0; k < primes.length; k++) {
+                for(int m = 2; m <= k/2; m++){
+                    if(primes[m] == true){
+                        if(k % m == 0){
+                            primes[k] = false;
+                            continue outer;
+                        }
+                    }
+                }
+            }
+
+
         System.out.println("\nCOMPUTING PRIME NUMBERS");
 
     }
 
     public static void displayPrimes(boolean primes[])
     {
-        System.out.println("\n\nPRIMES BETWEEN 1 AND "+ primes.length);
+        System.out.println("\nPRIMES BETWEEN 1 AND "+ primes.length);
         System.out.println();
+
+
+        DecimalFormat FourZeros = new DecimalFormat("0000");
+
+
+        int line = 1;
+
+        for(int k = 0; k < primes.length; k++) {
+            if(primes[k] == true) {
+                System.out.print(FourZeros.format(k) + " ");
+                if(line == 15){
+                    System.out.println();
+                    line = 0;
+                }
+                line++;
+            }
+        }
 
     }
 
